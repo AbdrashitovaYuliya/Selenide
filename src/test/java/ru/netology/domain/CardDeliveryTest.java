552a297710446cd7ee3410fa934fb42563de8938
AbdrashitovaYuliya <juli.lopatkina@gmail.com>
2023-02-08 21:54:39 +0300

    Created test

--- src/test/java/ru/netology/domain/CardDeliveryTest.java
+++ src/test/java/ru/netology/domain/CardDeliveryTest.java
@@ -1,7 +1,6 @@
 package ru.netology.domain;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,39 +10,35 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$x;
-import static org.openqa.selenium.Keys.BACK_SPACE;
+
 public class CardDeliveryTest {
+
     @BeforeEach
     void open() {
+
         Selenide.open("http://localhost:9999");
     }
 
-    public String generateDate(int days) {
-        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-// принимает на сколько дней сдвинуть дату и возвращает в нужном формате
-    }
+   public String generateDate(int addDays) {
+       return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+   }
 
     @Test
-    void succesForm() {
-        Configuration.holdBrowserOpen = true;
-
-        String planningDate = generateDate(4);
-
-        $("[data-test-id = city] input").setValue("Тамбов");
-        $("[data-test-id = date] .input__control").click();
-        $("[data-test-id = date] .input__control").sendKeys(Keys.CONTROL + "A");
-        $("[data-test-id = date] .input__control").sendKeys(BACK_SPACE);
-        $("[data-test-id = date] .input__control").setValue(planningDate);
-        $("[data-test-id = name] .input__control").setValue("Елена Банько");
-        $("[data-test-id = phone] .input__control").setValue("+79336657088");
+     public void shouldBeSuccessfullyCompleted() {
+        $("[data-test-id = city] input").setValue("Архангельск");
+        String currentDate = generateDate(7);
+        $("[data-test-id = date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id = date] input").sendKeys(currentDate);
+        $("[data-test-id = name] .input__control").setValue("Иван Белов");
+        $("[data-test-id = phone] .input__control").setValue("+79216666666");
         $("[data-test-id = agreement]").click();
-        $x("//button[contains(@class, 'button_view_extra')]").click();
-        $("[data-test-id = notification]").shouldBe(visible, Duration.ofSeconds(15));
-        $("[data-test-id = notification] [class='notification__content']")
-                .shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
+
+
     }
+    
 }
